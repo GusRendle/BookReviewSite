fed3dc4f08b1e05521d9ffc42be1c20428e16114
Gus Rendle <gus.rendle@protonmail.com>
2021-11-05 08:07:11 +0000

    Book seeder update

--- database/seeders/BookTableSeeder.php
+++ database/seeders/BookTableSeeder.php
@@ -14,6 +14,6 @@ class BookTableSeeder extends Seeder
      */
     public function run()
     {
-        $books = Book::factory()->count(100)->create();
+        $books = Book::factory()->count(30)->create();
     }
 }

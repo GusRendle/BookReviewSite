079a0076b7ec023f86a8c7a0ae442536431be293
Gus Rendle <gus.rendle@protonmail.com>
2021-12-16 18:22:54 +0000

    Fixed Book Controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,7 +9,7 @@ class BookController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except('index', 'show')
+        $this->middleware('auth')->except('index', 'show');
     }
     
     /**
